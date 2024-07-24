8d5654b9a2e8e2ccf4ad11b8227133337bab311f
Mrf3bo <calm.boat2479@fastmail.com>
2024-07-24 19:55:41 +0200

    Modified main.py to count and print out the number of words in the document.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)        
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_counter(text)
+    print(f"{num_words} words found in the document")
+    
+    
+## A function to count the words in the file that was read
+def word_counter(text):
+    words = text.split()
+    return len(words)
+
+## Read a file from the books directory and save it into a string variable
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
